0e3eecd3746bffff767eabb89ae0c2ce392f4dce
zyd_com <zyd_com@bafc0403-73ab-4def-ba9a-ad3b67c49236>
2012-03-15 03:48:18 +0000

    add gtest sample
    git-svn-id: http://code.taobao.org/svn/snippet@15 bafc0403-73ab-4def-ba9a-ad3b67c49236

---
+++ trunk/gtest/test_suite.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <gtest/gtest.h>
+
+int add(int a, int b)
+{
+  return a + b;
+}
+
+float addf(float a, float b)
+{
+  return a + b;
+}
+
+class AddTest: public testing::Test
+{
+public:
+  static void SetUpTestCase()
+  {
+    printf("test begin\n");
+  }
+  static void TearDownTestCase()
+  {
+    printf("test end\n");
+  }
+
+  AddTest()
+  {
+    printf("Test C\n");
+  }
+  ~AddTest()
+  {
+    printf("Test D\n");
+  }
+};
+
+TEST_F(AddTest, add_int)
+{
+  EXPECT_EQ(10, add(5, 5));
+  EXPECT_NE(10, add(5, 6));
+}
+
+TEST_F(AddTest, add_float)
+{
+  EXPECT_EQ(5.5, addf(2.25, 3.25));
+  EXPECT_NE(5.5, addf(2.25, 4.25));
+}
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
